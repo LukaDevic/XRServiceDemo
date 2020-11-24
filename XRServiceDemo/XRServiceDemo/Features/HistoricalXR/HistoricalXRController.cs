912470f1f0d15a6b0a66a1a7243b10b5319c4bf2
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2020-11-24 21:22:23 +0100

    Add controller

---
+++ XRServiceDemo/XRServiceDemo/Features/HistoricalXR/HistoricalXRController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using XRService.HistoricalXR.Application;
+using XRService.HistoricalXR.Domain;
+
+namespace XRServiceWeb.Features.HistoricalXR
+{
+    [Route("api/historicalxr")]
+    [ApiController]
+    public class HistoricalXRController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public HistoricalXRController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ExchangeRateInformation>> GetHistoricalExchangeRates([FromQuery] ListParameters parameters)
+        {
+            var result = await _mediator.Send(new GetHistoricalData.Query(parameters.Dates, parameters.BaseCurrency, parameters.TargetCurrency));
+
+            if (result.IsEmpty)
+            {
+                return NotFound();
+            }
+
+            return result.ExchangeRateInformation;
+        }
+    }
+}
