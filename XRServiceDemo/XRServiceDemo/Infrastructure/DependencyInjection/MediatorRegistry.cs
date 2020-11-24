b24192271200a67d75d4b64eb666ecb0e91dcfe5
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2020-11-24 21:22:53 +0100

    Add MediatorRegistry

---
+++ XRServiceDemo/XRServiceDemo/Infrastructure/DependencyInjection/MediatorRegistry.cs
@@ -0,0 +1,20 @@
+﻿using Lamar;
+using MediatR;
+
+namespace XRServiceWeb.Infrastructure.DependencyInjection
+{
+    public class MediatorRegistry : ServiceRegistry
+    {
+        public MediatorRegistry()
+        {
+            Scan(scanner =>
+            {
+                scanner.ConnectImplementationsToTypesClosing(typeof(IRequestHandler<,>));
+            });
+
+            For<IMediator>().Use<Mediator>().Transient();
+
+            For<ServiceFactory>().Use(ctx => ctx.GetInstance);
+        }
+    }
+}
